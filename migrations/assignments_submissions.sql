e9c8578b0fb70da2021cf3a1e47aad78ce0ae686
roylyh <roy.louyihui@gmail.com>
2023-03-06 20:14:55 -0800

    BootcampX Assignments

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,20 @@
+DROP TABLE assignment_submissions CASCADE;
+
+DROP TABLE assignments;
+
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  DAY INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
